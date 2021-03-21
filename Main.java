61dc9a921401701a7a4c2fec04571168c00c8047
JuanCarrera03 <car20593@uvg.edu.gt>
2021-03-21 21:53:51 +0000

    Agregaciond e clases y creacion de Maps

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Ingrese que tipo de Map se implementara:");
-        System.out.println("1. ");
+        System.out.println("1. HashMap");
         System.out.println("2. LinkedHashMap");
         System.out.println("3. TreeMap ");
     
